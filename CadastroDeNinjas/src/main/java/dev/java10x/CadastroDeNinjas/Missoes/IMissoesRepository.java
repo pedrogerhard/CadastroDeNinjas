4fdc4392e54e8542d9c8d6bcd025e1cb410dd525
Pedro Gerhard <pedrogerhardc@gmail.com>
2025-05-19 16:13:53 -0300

    fix: Adciona o @Collumn na minha classe

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/IMissoesRepository.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/IMissoesRepository.java
@@ -1,5 +1,4 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IMissoesRepository extends JpaRepository<MissoesModel, Long> {
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -17,10 +17,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column (name = "id")
     private Long id;
+
+    @Column (name = "nome")
     private String nome;
-    @Column(unique = true)
+
+    @Column (unique = true)
     private String email;
+
+    @Column (name = "ing_url")
+    private String ingUrl;
+
+    @Column (name = "idade")
     private int idade;
 
     // @ManyToOne - Um ninja tem uma unica missao
