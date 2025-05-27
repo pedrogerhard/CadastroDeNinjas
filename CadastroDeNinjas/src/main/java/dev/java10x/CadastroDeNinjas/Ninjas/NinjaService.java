f03ff8d044f1f938f8c00ff41ea5efc18dc50ef2
Pedro Gerhard <pedrogerhardc@gmail.com>
2025-05-27 15:38:53 -0300

    feat: Rota de findById adicionada

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -19,7 +20,8 @@ public class NinjaService {
     }
 
     // Listar por ID
-    public List<NinjaModel> listarNinjasId() {
-        return ;
+    public NinjaModel listarNinjasPorId(Long id) {
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
     }
 }
